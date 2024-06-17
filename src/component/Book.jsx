450afc0a6f0932aa96fadb6e2fffc4a4b63cf1b2
Noman0303 <nomanbutex38@gmail.com>
2024-06-17 17:22:34 +0600

    Home page done

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -1,17 +1,32 @@
 import PropTypes from 'prop-types';
-const Book = ({book}) => {
+import { FaRegStar } from "react-icons/fa";
+const Book = ({ book }) => {
 
-    const {bookId} = book;
+    const { image, tags, bookName, author, category, rating } = book;
 
-  return (
-    <div className="border-2 ">
-        <p>{bookId}</p>
+    return (
+        <div className='border-2 rounded-2xl p-6' >
+            <div className="card bg-[#F3F3F3]">
+                <figure><img className='h-56 m-4' src={image} alt="books image" /></figure>
+            </div>
+            <div className="card-actions justify-start mt-4">
+                <div className="badge badge-outline">{tags}</div>
+            </div>
+            <div className="mt-4 justify-start">
+                <h2 className="card-title">{bookName }</h2>
+                <p className='pb-5 border-dashed border-b-2'>By: {author}</p>
+            </div>
+            <div className='flex justify-between mt-4'>
+                <p>{category}</p>
+                <p className='flex' >{rating} <FaRegStar className='mt-1 ml-2'></FaRegStar> </p>
 
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 Book.propTypes = {
     book: PropTypes.object,
-    bookId : PropTypes.object,};
+    bookId: PropTypes.object,
+};
 export default Book
